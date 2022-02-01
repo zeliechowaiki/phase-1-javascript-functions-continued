8298a99f18af3b2139ad6dcc90b27ca69a822926
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-01-31 19:06:27 -0500

    commit tree e0030beb15d16d1fef5f1e6285cff9f4ae6ca9e3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(fun="roller-skate") {
+    return `This Saturday, I want to ${fun}!`
+}
+
+const mondayWork = function(work="go to the office") {
+    return `This Monday, I will ${work}.`
+}
+function wrapAdjective(wrapper="*") {
+    return function (adj="special") {
+        return `You are ${wrapper}${adj}${wrapper}!`;
+    }
+}
